53d5831d30cab1cc3335120897c3f3f2c55b04cf
prateekkish <prateekkish@gmail.com>
2016-05-18 00:45:10 +0530

    form based workflow completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,32 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show,:edit]
+  include ArticlesHelper
+  before_action :set_article, only: [:show,:edit, :update,:destroy]
   def index
     @articles = Article.all
   end
   def show
+  end
+  def new
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    flash.notice = "Article created successfully"
+    redirect_to article_path(@article)
+  end
+  def edit
+  end
+  def update
+    @article.update(article_params)
+    flash.notice = "Article #{@article.title} updated successfully!"
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+    flash.notice = "Article destroyed"
+    redirect_to articles_path
   end
 
   private
