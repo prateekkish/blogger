a212ef42e0255c27967c5ecc417bb23d5a121b45
prateekkish <prateekkish@gmail.com>
2016-05-18 00:08:16 +0530

    Initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,13 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show,:edit]
+  def index
+    @articles = Article.all
+  end
+  def show
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
